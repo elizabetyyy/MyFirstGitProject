8be552ba226797acce46aaae06cf48eee05eead2
Криворучко Єлизавета <elizabeth.kryvoruchko@krfk.kai.edu.ua>
2025-02-19 21:39:21 +0200

    Перша версія програми

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MyFirstGitProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Моє ім'я: Ліза, група: 3-027");
+        }
+    }
+}
